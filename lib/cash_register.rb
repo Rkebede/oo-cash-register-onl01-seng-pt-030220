cb86b56b623c93f7ca2472f02603b0499fb57982
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-17 00:37:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,11 +13,13 @@ class CashRegister
   end 
   
   def apply_discount
-    discount = @discount.to_f / 100
-    @total -= (@total * discount)
-    return "After the discount, the total comes to $#{@total.to_i}."
-    
-    
+    if @discount == 0
+      return "There is no discount to apply."
+    else 
+      discount = @discount.to_f / 100
+      @total -= (@total * discount)
+      return "After the discount, the total comes to $#{@total.to_i}."
+    end 
   end
  
 end 
